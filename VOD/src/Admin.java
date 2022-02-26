2076411a9d891031aad2ef3ccb5a6e043bd73403
GalSeidner <galseidner@gmail.com>
2022-02-26 14:50:18 +0200

    Added infrastructure for the Builder pattern, being used when admin should add new movie to DB. Added order class, need to discuss that.

--- VOD/src/Admin.java
+++ VOD/src/Admin.java
@@ -1,11 +1,21 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Admin extends User{
 
     public void addMovie() {
-
+        Movie newMovie = Movie.startBuild().
+                movieTitle("title").
+                category("action").
+                year(2022).
+                actors(new String[]{"first actor", "second actor", "third actor"}).
+                isAvailable(true).
+                popularity(5).
+                build();
+        Db.addMovie(newMovie);
     }
 
     public void removeMovie() {
-
     }
 
     public void editMovie() {
--- VOD/src/Movie.java
+++ VOD/src/Movie.java
@@ -11,13 +11,25 @@ public class Movie {
 
     public Movie(String movieTitle, String category, int year, String[] actors,
                  boolean isAvailable,  double inc_popularity) {
-
         this.title = movieTitle;
         this.category = category;
         this.year = year;
         this.actors = actors;
         this.isAvailable = isAvailable;
         this.popularity = inc_popularity;
+    }
+
+    public Movie(MovieBuilder builder){
+        this.title = builder.getTitle();
+        this.category = builder.getCategory();
+        this.year = builder.getYear();
+        this.actors = builder.getActors();
+        this.isAvailable = builder.getIsAvailable();
+        this.popularity = builder.getPopularity();
+    }
+
+    public static MovieTitle startBuild(){
+        return new MovieBuilder();
     }
 
     public String getTitle() {
